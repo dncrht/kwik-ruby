b1eccfe5bf6665ce6ede79bde2a2b49b8235bb22
Dani <daniel.cr.ht@gmail.com>
2017-04-25 10:01:38 +0200

    Avoid repetition

--- app/repositories/repo/filesystem.rb
+++ app/repositories/repo/filesystem.rb
@@ -2,7 +2,7 @@ class Repo
   class Filesystem
 
     def find_by(model, criteria)
-      path = path_for(model.name.tableize) << criteria[:name]
+      path = path_for(model.name) << criteria[:name]
       return unless File.exist?(path)
       file = File.open path, 'r'
       content = file.read
@@ -11,7 +11,7 @@ class Repo
     end
 
     def save(entity)
-      directory = path_for(entity.class.name.tableize)
+      directory = path_for(entity.class.name)
       FileUtils.mkdir_p directory #mkdir_p avoids exception if directory exists
 
       path = directory << entity.name
@@ -21,7 +21,7 @@ class Repo
     end
 
     def destroy(entity)
-      path = path_for(entity.class.name.tableize) << entity.name
+      path = path_for(entity.class.name) << entity.name
       File.delete(path) if File.exist?(path) && entity.name != Rails.application.config.MAIN_PAGE
     end
 
@@ -30,12 +30,12 @@ class Repo
       content = criteria[:content]
 
       if name.present?
-        return Dir.entries(path_for(model.name.tableize)).sort.map do |file|
+        return Dir.entries(path_for(model.name)).sort.map do |file|
           file if file.downcase.include? name.downcase
         end.compact.tap { |search_names| search_names << name if name == Rails.application.config.ALL_PAGE }
       elsif content.present?
         search_content = Hash.new { |h, k| h[k] = [] }
-        results = `cd "#{path_for(model.name.tableize)}"; grep '#{content}' *` #TODO case insensitive search
+        results = `cd "#{path_for(model.name)}"; grep '#{content}' *` #TODO case insensitive search
         results.split("\n").each do |result|
           page, matching_line = result.split(':', 2)
           search_content[page] << matching_line << "\n"
@@ -47,7 +47,7 @@ class Repo
     end
 
     def all(model)
-      Dir.entries(path_for(model.name.tableize)).sort.map do |file|
+      Dir.entries(path_for(model.name)).sort.map do |file|
         if file[0, 1] != '.' && file != Rails.application.config.MAIN_PAGE
           file
         end
